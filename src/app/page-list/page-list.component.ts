f04233847c216efb2965baba2ca6b3da21cb619c
Babak <babak.jahanbani@yahoo.com>
2022-03-19 15:04:47 +0330

    Make titles linkable & snippet readable in HTML

--- src/app/page-list/page-list.component.ts
+++ src/app/page-list/page-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+type NewType = any;
+
 @Component({
   selector: 'app-page-list',
   templateUrl: './page-list.component.html',
   styleUrls: ['./page-list.component.css'],
 })
 export class PageListComponent implements OnInit {
-  @Input() posts: any = [];
+  @Input() posts: NewType = [];
 
   constructor() {}
 
